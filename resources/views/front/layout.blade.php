d1bd1a123a66375cdf9f593a8d98e8b9498e3335
Abnayem <nayemabu926@gmail.com>
2023-09-23 01:05:48 +0600

    seventh commit

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -68,7 +68,12 @@
                   
                   <li class="hidden-xs"><a href="{{url('/cart')}}">My Cart</a></li>
                   <li class="hidden-xs"><a href="javascript:void(0)">Checkout</a></li>
+                  @if(session()->has('FRONT_USER_LOGIN')!=null)
+                  <li><a href="{{url('logout')}}">Logout</a></li>
+                  @else
+                  
                   <li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
+                @endif
                 </ul>
               </div>
             </div>
@@ -285,17 +290,28 @@
         <div class="modal-body">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
           <h4>Login or Register</h4>
-          <form class="aa-login-form" action="">
-            <label for="">Username or Email address<span>*</span></label>
-            <input type="text" placeholder="Username or email">
+          <form class="aa-login-form" id="frmLogin" >
+            
+          <label for="">Email address<span>*</span></label>
+           
+            <input type="email" placeholder="email" name="str_login_email" required>
+            
             <label for="">Password<span>*</span></label>
-            <input type="password" placeholder="Password">
-            <button class="aa-browse-btn" type="submit">Login</button>
+
+            <input type="password" placeholder="Password" name="str_login_password" required>
+
+            <button class="aa-browse-btn" type="submit" id="btnLogin">Login</button>
+            
             <label for="rememberme" class="rememberme"><input type="checkbox" id="rememberme"> Remember me </label>
+            
+            <div id="login_msg" style="clear:both; color:red;"></div>
+            
             <p class="aa-lost-password"><a href="#">Lost your password?</a></p>
+            
             <div class="aa-register-now">
               Don't have an account?<a href="{{url('registration')}}">Register now!</a>
             </div>
+            @csrf
           </form>
         </div>                        
       </div><!-- /.modal-content -->
